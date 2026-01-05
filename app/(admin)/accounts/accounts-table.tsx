97efcbeea0d0ddf055d981b18d97af9999366b87
josueaguilarr <josue.aguilar225@gmail.com>
2026-01-05 15:51:34 -0500

    fix(accounts-table): update import statements for type safety and correct paths

--- app/(admin)/accounts/accounts-table.tsx
+++ app/(admin)/accounts/accounts-table.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Account, columns } from "./columns";
-import { DataTable } from "./data-table";
+import { type Account, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
 
 export default function AccountsTable({ data }: { data: Account[] }) {
   return <DataTable columns={columns} data={data} />;
